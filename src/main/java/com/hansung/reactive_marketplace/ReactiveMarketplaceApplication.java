96ddc5f91a7296f857052beebea41c515e20bd4c
hansung1908 <hansung1908@naver.com>
2024-09-18 23:06:19 +0900

    프로젝트 생성

---
+++ src/main/java/com/hansung/reactive_marketplace/ReactiveMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.hansung.reactive_marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveMarketplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hansung/reactive_marketplace/ReactiveMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hansung.reactive_marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
