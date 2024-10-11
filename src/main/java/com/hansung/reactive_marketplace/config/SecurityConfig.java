37ce460984076b5a5ff0c25190ccba3e4e7ce53b
hansung1908 <hansung1908@naver.com>
2024-10-11 23:44:16 +0900

    회원 로그인 백엔드 구성 (시큐리티 설정 해결, 프론트 x)

--- src/main/java/com/hansung/reactive_marketplace/config/SecurityConfig.java
+++ src/main/java/com/hansung/reactive_marketplace/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.hansung.reactive_marketplace.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -20,8 +21,11 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         http.csrf(csrfSpec -> csrfSpec.disable())
+                .anonymous(Customizer.withDefaults()) // thymeleaf spring security에서 anonymous 사용을 위한 활성화
                 .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec
-                        .anyExchange().permitAll());
+                        .pathMatchers("/", "/js/**", "/user/**").permitAll()
+                        .pathMatchers("/products").permitAll()
+                        .anyExchange().authenticated());
 
 //                .formLogin(formLoginSpec -> formLoginSpec
 //                        .loginPage("/user/loginForm")
