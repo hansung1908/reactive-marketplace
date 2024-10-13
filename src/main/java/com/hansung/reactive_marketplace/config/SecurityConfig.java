365e57c2ff111082af5a516b1c08e4384a88b0e6
hansung1908 <hansung1908@naver.com>
2024-10-13 18:00:54 +0900

    회원 로그인 구현 (커스텀은 실패)

--- src/main/java/com/hansung/reactive_marketplace/config/SecurityConfig.java
+++ src/main/java/com/hansung/reactive_marketplace/config/SecurityConfig.java
@@ -1,10 +1,15 @@
 package com.hansung.reactive_marketplace.config;
 
+import com.hansung.reactive_marketplace.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.RedirectServerAuthenticationSuccessHandler;
@@ -27,9 +32,7 @@ public class SecurityConfig {
                         .pathMatchers("/products", "/login").permitAll()
                         .anyExchange().authenticated())
 
-                .formLogin(formLoginSpec -> formLoginSpec
-                        .loginPage("/user/loginForm")
-                        .authenticationSuccessHandler(new RedirectServerAuthenticationSuccessHandler("/")));
+                .formLogin(Customizer.withDefaults());
 
         return http.build();
     }
--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -25,40 +25,11 @@ async function saveUser(event) {
     }
 }
 
-async function loginUser(event) {
-    event.preventDefault(); // 폼 제출 기본 동작 방지
-
-    try {
-        const data = {
-            username: document.querySelector('#username').value,
-            password: document.querySelector('#password').value
-        }
-
-        const response = await fetch("/login", {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        });
-
-        if(response.ok) {
-            window.location.href = '/';
-        }
-    } catch (error) {
-        console.error('Error:', error); // 오류 처리
-    }
-}
-
 document.addEventListener('DOMContentLoaded', () => {
     // 현재 페이지 URL 경로 가져오기
     const currentPath = window.location.pathname;
 
     if (currentPath === '/user/saveForm') {
         document.getElementById('user-save').addEventListener('submit', saveUser); // 폼 제출 이벤트 리스너 등록
-    }
-
-    else if (currentPath === '/user/loginForm') {
-        document.getElementById('user-login').addEventListener('submit', loginUser);
     }
 });
