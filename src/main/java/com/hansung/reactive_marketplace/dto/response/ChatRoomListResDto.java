d784e91cdb825b3f886085bc9feceb0984b275b8
hansung1908 <hansung1908@naver.com>
2024-10-31 21:09:50 +0900

    1:1 채팅방 리스트 구현

---
+++ src/main/java/com/hansung/reactive_marketplace/dto/response/ChatRoomListResDto.java
@@ -0,0 +1,33 @@
+package com.hansung.reactive_marketplace.dto.response;
+
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class ChatRoomListResDto {
+
+    private String productId;
+
+    private String productTitle;
+
+    private String seller;
+
+    private String buyer;
+
+    private String recentMsg;
+
+    private LocalDateTime recentCreatedAt;
+
+    protected ChatRoomListResDto() {
+    }
+
+    public ChatRoomListResDto(String productId, String productTitle, String seller, String buyer, String recentMsg, LocalDateTime recentCreatedAt) {
+        this.productId = productId;
+        this.productTitle = productTitle;
+        this.seller = seller;
+        this.buyer = buyer;
+        this.recentMsg = recentMsg;
+        this.recentCreatedAt = recentCreatedAt;
+    }
+}
--- src/main/java/com/hansung/reactive_marketplace/repository/ChatRepository.java
+++ src/main/java/com/hansung/reactive_marketplace/repository/ChatRepository.java
@@ -1,14 +1,23 @@
 package com.hansung.reactive_marketplace.repository;
 
 import com.hansung.reactive_marketplace.domain.Chat;
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.data.mongodb.repository.Tailable;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface ChatRepository extends ReactiveMongoRepository<Chat, String> {
 
     @Tailable
     @Query(value = "{ 'roomId': ?0 }")
     Flux<Chat> findMsgByRoomId(String roomId);
+
+    @Aggregation(pipeline = {
+            "{ '$match': { 'roomId': ?0 } }",
+            "{ '$sort': { 'createdAt': -1 } }",
+            "{ '$limit': 1 }"
+    })
+    Mono<Chat> findRecentChat(String roomId);
 }
