b3afed0974c9928bc3895a0f679bb7f5a0307dfa
hansung1908 <hansung1908@naver.com>
2024-11-21 23:58:14 +0900

    reponse dto service에서 dto 변환 사용으로 비즈니스 로직 분리

--- src/main/java/com/hansung/reactive_marketplace/repository/ProductRepository.java
+++ src/main/java/com/hansung/reactive_marketplace/repository/ProductRepository.java
@@ -2,8 +2,6 @@ package com.hansung.reactive_marketplace.repository;
 
 import com.hansung.reactive_marketplace.domain.Product;
 import com.hansung.reactive_marketplace.domain.ProductStatus;
-import com.hansung.reactive_marketplace.dto.response.MyProductListResDto;
-import com.hansung.reactive_marketplace.dto.response.ProductListResDto;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
@@ -14,10 +12,10 @@ import reactor.core.publisher.Mono;
 public interface ProductRepository extends ReactiveMongoRepository<Product, String> {
 
     @Query(value = "{}", fields = "{ '_id' : 1, 'title' : 1, 'price' : 1 }")
-    Flux<ProductListResDto> findProductList(Sort sort);
+    Flux<Product> findProductList(Sort sort);
 
     @Query(value = "{ 'userId' : ?0 }", fields = "{ '_id' : 1, 'title' : 1, 'description' : 1, 'price' : 1, 'status' : 1, 'createdAt' : 1 }")
-    Flux<MyProductListResDto> findMyProductList(String userId, Sort sort);
+    Flux<Product> findMyProductList(String userId, Sort sort);
 
     @Query(value = "{ '_id' : ?0 }")
     @Update("{ '$set' : { 'description' : ?1, 'price' : ?2, 'status' : ?3 }}")
