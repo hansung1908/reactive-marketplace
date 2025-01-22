f2d3e1729122710643d6191376ae8ec3e5c96409
hansung1908 <hansung1908@naver.com>
2025-01-22 23:59:55 +0900

    userService 전반적인 코드 수정중 (리액티브 프로그래밍 지키며 수정)

--- src/main/java/com/hansung/reactive_marketplace/service/UserServiceImpl.java
+++ src/main/java/com/hansung/reactive_marketplace/service/UserServiceImpl.java
@@ -15,6 +15,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+import java.util.Optional;
+
 @Service
 public class UserServiceImpl implements UserService {
 
@@ -33,11 +35,9 @@ public class UserServiceImpl implements UserService {
 
     public Mono<User> saveUser(UserSaveReqDto userSaveReqDto, FilePart image) {
         return userRepository.existsByUsername(userSaveReqDto.username())
-                .flatMap(exist -> {
-                    if (exist) { // 중복이 있으면 error
-                        return Mono.error(new ApiException(ExceptionMessage.USERNAME_ALREADY_EXISTS));
-                    }
-
+                .filter(exist -> !exist) // 중복이 있는 상태를 empty 상태로 변환
+                .switchIfEmpty(Mono.error(new ApiException(ExceptionMessage.USERNAME_ALREADY_EXISTS)))
+                .flatMap(nonExist -> { // 없으면 회원가입 진행
                     User user = new User.Builder()
                             .username(userSaveReqDto.username())
                             .nickname(userSaveReqDto.nickname())
@@ -45,15 +45,15 @@ public class UserServiceImpl implements UserService {
                             .email(userSaveReqDto.email())
                             .build();
 
-                    return userRepository.save(user) // 없으면 회원가입 진행
-                            .flatMap(savedUser -> {
-                                if (image != null) {
-                                    return imageService.uploadImage(image, savedUser.getId(), userSaveReqDto.imageSource())
-                                            .thenReturn(savedUser);
-                                }
-                                return Mono.just(savedUser);
-                            });
-                });
+                    return userRepository.save(user)
+                            .flatMap(savedUser ->
+                                    Mono.justOrEmpty(image)
+                                            .flatMap(img -> imageService.uploadImage(image, savedUser.getId(), userSaveReqDto.imageSource())
+                                                    .thenReturn(savedUser))
+                                            .switchIfEmpty(Mono.just(savedUser))
+                            );
+                })
+                .onErrorMap(e -> new ApiException(ExceptionMessage.INTERNAL_SERVER_ERROR));
     }
 
     public Mono<User> findUserByUsername(String username) {
@@ -69,7 +69,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public Mono<UserProfileResDto> findUserProfile(Authentication authentication) {
         return imageService.findProfileImageById(AuthUtils.getAuthenticationUser(authentication).getId())
-                .switchIfEmpty(Mono.error(new ApiException(ExceptionMessage.USER_NOT_FOUND)))
                 .flatMap(image -> findUserById(AuthUtils.getAuthenticationUser(authentication).getId())
                         .map(user -> new UserProfileResDto(
                                 user.getUsername(),
@@ -77,43 +76,39 @@ public class UserServiceImpl implements UserService {
                                 user.getEmail(),
                                 image.getImagePath()
                         )))
-                .onErrorResume(e -> e instanceof ApiException ? Mono.error(e) :
-                        Mono.error(new ApiException(ExceptionMessage.INTERNAL_SERVER_ERROR)));
+                .onErrorMap(e -> new ApiException(ExceptionMessage.USER_NOT_FOUND));
     }
 
     public Mono<Void> updateUser(UserUpdateReqDto userUpdateReqDto, FilePart image) {
         return userRepository.findById(userUpdateReqDto.id())
                 .flatMap(user -> {
-                    String password = user.getPassword();
+                    String password = Optional.ofNullable(userUpdateReqDto.password())
+                            .filter(pwd -> !pwd.isEmpty())
+                            .map(pwd -> bCryptPasswordEncoder.encode(pwd))
+                            .orElse(user.getPassword());
 
-                    if (userUpdateReqDto.password() != null && !userUpdateReqDto.password().isEmpty()) {
-                        password = bCryptPasswordEncoder.encode(userUpdateReqDto.password());
-                    }
-
-                    Mono<Void> updateUserMono = userRepository.updateUser(
+                    return userRepository.updateUser(
                             userUpdateReqDto.id(),
                             userUpdateReqDto.nickname(),
                             password,
                             userUpdateReqDto.email()
+                    ).flatMap(updateResult ->
+                            Mono.justOrEmpty(image)
+                                    .flatMap(img ->
+                                            imageService.deleteProfileImageById(userUpdateReqDto.id())
+                                                    .then(imageService.uploadImage(image, userUpdateReqDto.id(), userUpdateReqDto.imageSource()))
+                                    )
+                                    .switchIfEmpty(Mono.empty())
+                                    .then(Mono.just(updateResult))
                     );
-
-                    if (image != null) {
-                        return updateUserMono.then(
-                                imageService.deleteProfileImageById(userUpdateReqDto.id())
-                                        .then(imageService.uploadImage(image, userUpdateReqDto.id(), userUpdateReqDto.imageSource()))
-                                        .then() // Mono<Void> 반환을 위한 then
-                        );
-                    } else {
-                        return updateUserMono;
-                    }
-                });
+                })
+                .onErrorMap(e -> new ApiException(ExceptionMessage.INTERNAL_SERVER_ERROR));
     }
 
     public Mono<Void> deleteUser(UserDeleteReqDto userDeleteReqDto) {
-        return userRepository.findById(userDeleteReqDto.id())
+        return userRepository.findById(userDeleteReqDto.id()) // deleteById는 id 유무와 상관없이 실행하므로 체크 필요
                 .switchIfEmpty(Mono.error(new ApiException(ExceptionMessage.USER_NOT_FOUND)))
                 .flatMap(user -> userRepository.deleteById(user.getId()))
-                .onErrorResume(e -> e instanceof ApiException ? Mono.error(e) :
-                        Mono.error(new ApiException(ExceptionMessage.INTERNAL_SERVER_ERROR)));
+                .onErrorMap(e -> new ApiException(ExceptionMessage.INTERNAL_SERVER_ERROR));
     }
 }
