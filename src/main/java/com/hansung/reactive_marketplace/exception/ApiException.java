8a8c7f8a40dfb114eb60e6074a7112251f673ca2
hansung1908 <hansung1908@naver.com>
2025-01-22 23:59:55 +0900

    userService 전반적인 코드 수정중 (리액티브 프로그래밍 지키며 수정)

--- src/main/java/com/hansung/reactive_marketplace/exception/ApiException.java
+++ src/main/java/com/hansung/reactive_marketplace/exception/ApiException.java
@@ -1,13 +1,12 @@
 package com.hansung.reactive_marketplace.exception;
 
+import lombok.Getter;
+
+@Getter
 public class ApiException extends RuntimeException {
     private final ExceptionMessage exception;
 
     public ApiException(ExceptionMessage exception) {
         this.exception = exception;
-    }
-
-    public ExceptionMessage getException() {
-        return exception;
     }
 }
