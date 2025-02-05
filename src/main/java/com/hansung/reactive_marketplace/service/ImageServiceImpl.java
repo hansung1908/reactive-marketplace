df1632733ca190e17a5f3f75ba394d6868979131
hansung1908 <hansung1908@naver.com>
2025-02-05 23:38:44 +0900

    imageService 최적화 (캐싱 필요)

--- src/main/java/com/hansung/reactive_marketplace/service/ImageServiceImpl.java
+++ src/main/java/com/hansung/reactive_marketplace/service/ImageServiceImpl.java
@@ -13,12 +13,11 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 @Service
-public class ImageServiceImpl implements ImageService{
+public class ImageServiceImpl implements ImageService {
 
     private final ImageRepository imageRepository;
 
@@ -39,67 +38,51 @@ public class ImageServiceImpl implements ImageService{
     }
 
     public Mono<Image> uploadImage(FilePart image, String id, ImageSource imageSource) {
-        if (image == null) {
-            return Mono.error(new ApiException(ExceptionMessage.IMAGE_UPLOAD_FAILED));
-        }
+        return Mono.justOrEmpty(image)
+                .switchIfEmpty(Mono.error(new ApiException(ExceptionMessage.IMAGE_NOT_FOUND)))
+                .flatMap(img -> createImageData(img, id, imageSource))
+                .flatMap(imageData -> saveImageFiles(image, imageData, imageSource))
+                .flatMap(imageData -> imageRepository.save(imageData))
+                .onErrorMap(e -> !(e instanceof ApiException),
+                        e -> new ApiException(ExceptionMessage.IMAGE_UPLOAD_FAILED));
+    }
 
+    private Mono<Image> createImageData(FilePart image, String id, ImageSource imageSource) {
         String imageName = ImageUtils.generateUniqueImageName(image.filename());
 
-        Image imageData = new Image.Builder()
+        return Mono.just(new Image.Builder()
                 .imageName(image.filename())
                 .imageType(String.valueOf(image.headers().getContentType()))
                 .imageSize(image.headers().getContentLength())
-                .build();
+                .imageSource(imageSource)
+                .userId(imageSource == ImageSource.PROFILE ? id : null)
+                .productId(imageSource == ImageSource.PRODUCT ? id : null)
+                .imagePath(ImageUtils.generateImagePath(
+                        imageSource == ImageSource.PROFILE ? profileOriginalPath : productOriginalPath,
+                        imageName
+                ))
+                .thumbnailPath(ImageUtils.generateImagePath(
+                        imageSource == ImageSource.PROFILE ? profileThumbnailPath : productThumbnailPath,
+                        "resized_" + imageName
+                ))
+                .build());
+    }
 
-        switch (imageSource) { // 이미지 타입에 따라 분류
-            case PROFILE -> {
-                imageData = imageData.toBuilder()
-                        .imageSource(ImageSource.PROFILE)
-                        .userId(id)
-                        .imagePath(ImageUtils.generateImagePath(profileOriginalPath, imageName))
-                        .thumbnailPath(ImageUtils.generateImagePath(profileThumbnailPath, "resized_" + imageName))
-                        .build();
+    private Mono<Image> saveImageFiles(FilePart image, Image imageData, ImageSource imageSource) {
+        File originalFile = new File(imageData.getImagePath());
+        File resizedFile = new File(imageData.getThumbnailPath());
 
-                File originalProfileImage = new File(profileOriginalPath, imageName); // 원본 이미지 파일 경로
-                File resizedProfileImage = new File(profileThumbnailPath, "resized_" + imageName); // 리사이즈된 이미지 파일 경로
-
-                try {
-                    image.transferTo(originalProfileImage).subscribe(); // 원본 이미지 업로드
-
-                    Thumbnails.of(originalProfileImage) // 해당 경로로부터 이미지 가져옴
-                            .forceSize(45, 45) // 이미지 크기를 리사이즈
-                            .outputQuality(0.8) // 품질을 80%로 설정 (0.0 ~ 1.0 범위)
-                            .toFile(resizedProfileImage); // 리사이즈된 이미지 업로드
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-            case PRODUCT -> {
-                imageData = imageData.toBuilder()
-                        .imageSource(ImageSource.PRODUCT)
-                        .productId(id)
-                        .imagePath(ImageUtils.generateImagePath(productOriginalPath, imageName))
-                        .thumbnailPath(ImageUtils.generateImagePath(productThumbnailPath, "resized_" + imageName))
-                        .build();
-
-                File originalProductImage = new File(productOriginalPath, imageName);
-                File resizedProductImage = new File(productThumbnailPath, "resized_" + imageName);
-
-                try {
-                    image.transferTo(originalProductImage).subscribe();
-
-                    Thumbnails.of(originalProductImage)
-                            .forceSize(300, 350)
+        return image.transferTo(originalFile)
+                .then(Mono.fromCallable(() -> {
+                    Thumbnails.of(originalFile)
+                            .forceSize(
+                                    imageSource == ImageSource.PROFILE ? 45 : 300,
+                                    imageSource == ImageSource.PROFILE ? 45 : 350
+                            )
                             .outputQuality(0.8)
-                            .toFile(resizedProductImage);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        }
-
-        return imageRepository.save(imageData)
-                .onErrorMap(e -> new ApiException(ExceptionMessage.IMAGE_UPLOAD_FAILED));
+                            .toFile(resizedFile);
+                    return imageData;
+                }));
     }
 
     public Mono<Image> findProductImageById(String productId) {
@@ -110,24 +93,16 @@ public class ImageServiceImpl implements ImageService{
     public Mono<Image> findProfileImageById(String userId) {
         return imageRepository.findByUserId(userId)
                 .switchIfEmpty(Mono.fromCallable(() -> new Image.Builder() // 이미지가 없으면 기본 이미지로 대체
-                                    .imagePath("/img/profile.png")
-                                    .thumbnailPath("/img/profile.png")
-                                    .build())
-                            );
+                        .imagePath("/img/profile.png")
+                        .thumbnailPath("/img/profile.png")
+                        .build())
+                );
     }
 
     public Mono<Void> deleteProductImageById(String productId) {
         return imageRepository.findByProductId(productId)
                 .switchIfEmpty(Mono.error(new ApiException(ExceptionMessage.IMAGE_NOT_FOUND)))
-                .flatMap(image -> {
-                    try {
-                        Files.delete(Paths.get(image.getImagePath()));
-                        Files.delete(Paths.get(image.getThumbnailPath()));
-                        return Mono.just(image);
-                    } catch (IOException e) {
-                        return Mono.error(new ApiException(ExceptionMessage.IMAGE_DELETE_FAILED));
-                    }
-                })
+                .flatMap(image -> deleteImageFiles(image))
                 .then(imageRepository.deleteByProductId(productId))
                 .onErrorMap(e -> !(e instanceof ApiException),
                         e -> new ApiException(ExceptionMessage.IMAGE_DELETE_FAILED));
@@ -136,17 +111,17 @@ public class ImageServiceImpl implements ImageService{
     public Mono<Void> deleteProfileImageById(String userId) {
         return imageRepository.findByUserId(userId)
                 .switchIfEmpty(Mono.error(new ApiException(ExceptionMessage.IMAGE_NOT_FOUND)))
-                .flatMap(image -> {
-                    try {
-                        Files.delete(Paths.get(image.getImagePath()));
-                        Files.delete(Paths.get(image.getThumbnailPath()));
-                        return Mono.just(image);
-                    } catch (IOException e) {
-                        return Mono.error(new ApiException(ExceptionMessage.IMAGE_DELETE_FAILED));
-                    }
-                })
+                .flatMap(image -> deleteImageFiles(image))
                 .then(imageRepository.deleteByUserId(userId))
                 .onErrorMap(e -> !(e instanceof ApiException),
                         e -> new ApiException(ExceptionMessage.IMAGE_DELETE_FAILED));
+    }
+
+    private Mono<Image> deleteImageFiles(Image image) {
+        return Mono.fromCallable(() -> {
+                    Files.delete(Paths.get(image.getImagePath()));
+                    Files.delete(Paths.get(image.getThumbnailPath()));
+                    return image;
+                });
     }
 }
