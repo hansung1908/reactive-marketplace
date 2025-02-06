caa0c4c882c37d1dbe798e2a2fcc4af1f9a7cb95
hansung1908 <hansung1908@naver.com>
2025-02-06 23:56:00 +0900

    imageService 캐시 적용중 (캐시 사용하면 안되는 곳에서 find 메소드 사용)

--- src/main/java/com/hansung/reactive_marketplace/domain/Image.java
+++ src/main/java/com/hansung/reactive_marketplace/domain/Image.java
@@ -1,5 +1,9 @@
 package com.hansung.reactive_marketplace.domain;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
@@ -27,6 +31,8 @@ public class Image {
 
     private String imagePath;
 
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     @CreatedDate
     private LocalDateTime createdAt;
 
