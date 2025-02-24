0c7e9d26325d8d4e166d7a5fe54d2e585c553b63
hansung1908 <hansung1908@naver.com>
2025-02-24 23:59:12 +0900

    mongodb 트랜잭션 설정을 위한 레플리카 셋 설정

--- src/main/java/com/hansung/reactive_marketplace/ReactiveMarketplaceApplication.java
+++ src/main/java/com/hansung/reactive_marketplace/ReactiveMarketplaceApplication.java
@@ -2,7 +2,6 @@ package com.hansung.reactive_marketplace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 public class ReactiveMarketplaceApplication {
